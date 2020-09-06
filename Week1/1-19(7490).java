dc8e786ddae3a55ae6fb214bb7bc2bcdffcce77f
Jacob An <lightyear.thespaceranger@gmail.com>
2020-09-06 23:11:03 +0900

    unSOLVED!

---
+++ Week1/1-19(7490).java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+        }
+    }
+
+    public static void func(int N, int sum, int index, char operator, StringBuilder sb) {
+        if(index == N) {
+            if (sum == 0)
+                System.out.print();
+        }
+    }
+}
