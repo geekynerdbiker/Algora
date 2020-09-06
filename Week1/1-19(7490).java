646451b1ff2da12ef5ca8275544960d13181f5ef
Jacob An <lightyear.thespaceranger@gmail.com>
2020-09-06 23:35:38 +0900

    SOLVED!

--- Week1/1-19(7490).java
+++ Week1/1-19(7490).java
@@ -10,14 +10,21 @@ public class Main {
 
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
-
+            func(0, 1, 1, 1, N, "1");
+            System.out.println();
         }
     }
 
-    public static void func(int N, int sum, int index, char operator, StringBuilder sb) {
-        if(index == N) {
+    public static void func(int sum, int op, int next, int prev, int N, String s) {
+        if (prev == N) {
+            sum += (next * op);
             if (sum == 0)
-                System.out.print();
+                System.out.println(s);
+            return;
         }
+
+        func(sum, op, next * 10 + prev + 1, prev + 1, N, s + ' ' + (prev + 1));
+        func(sum + op * next, 1, prev + 1, prev + 1, N, s + '+' + (prev + 1));
+        func(sum + op * next, -1, prev + 1, prev + 1, N, s + '-' + (prev + 1));
     }
 }
