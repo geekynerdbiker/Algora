311dd21ed9ce97b9dcf2ecab0e41516a02e731d4
Jacob An <lightyear.thespaceranger@gmail.com>
2020-09-13 19:39:29 +0900

    SOLVED!

--- Workbook/1-2/5(1032).java
+++ Workbook/1-2/5(1032).java
@@ -1,12 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a = kb.nextInt();
-        a += kb.nextInt();
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        String[] inputs = new String[N];
+        char[] result = new char[50];
 
-        System.out.println(a);
+        for (int i = 0; i < N; i++) {
+            inputs[i] = br.readLine();
+            if (i == 0) result = inputs[i].toCharArray();
+        }
+
+        for (int i = 0; i < N; i++) {
+            char [] string = inputs[i].toCharArray();
+            for (int j = 0; j < inputs[i].length(); j++) {
+                if (result[j] != string[j]) result[j] = '?';
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < result.length; i++)
+            sb.append(result[i]);
+
+        bw.write(sb.toString());
+        bw.close();
     }
 }
