6ac1a84952468b21c64d64fb851edf7c2c494154
Jacob An <lightyear.thespaceranger@gmail.com>
2020-10-05 22:53:11 +0900

    unSOLVED

---
+++ Week41/41-2(17502).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        char[] word = br.readLine().toCharArray();
+
+        for (int i = 0; i < N / 2 + 1; i++) {
+
+        }
+    }
+}
