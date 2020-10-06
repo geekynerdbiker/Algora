f049876aafaf80ee9c6fab5862891b01fcd4c21a
Jacob An <lightyear.thespaceranger@gmail.com>
2020-10-06 16:26:29 +0900

    SOLVED!

--- Week41/41-2(17502).java
+++ Week41/41-2(17502).java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,7 +8,17 @@ public class Main {
         char[] word = br.readLine().toCharArray();
 
         for (int i = 0; i < N / 2 + 1; i++) {
-
+            if (word[i] == '?' || word[N - i - 1] == '?') {
+                if (word[i] == '?' && word[N - i - 1] == '?') {
+                    word[i] = 'a';
+                    word[N - i - 1] = 'a';
+                } else if (word[i] == '?')
+                    word[i] = word[N - i - 1];
+                else word[N - i - 1] = word[i];
+            }
         }
+
+        for (int i = 0; i < N; i++)
+            System.out.print(word[i]);
     }
 }
